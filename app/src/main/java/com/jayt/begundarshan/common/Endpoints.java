9f96f41d145c7306e42045953a452a5eedb6f8d6
Jay Tailor <jayt@unity3d.com>
2018-09-12 13:07:03 -0700

    fixed the port issue

--- app/src/main/java/com/jayt/begundarshan/common/Endpoints.java
+++ app/src/main/java/com/jayt/begundarshan/common/Endpoints.java
@@ -2,5 +2,5 @@ package com.jayt.begundarshan.common;
 
 public class Endpoints {
 
-    public static final String SERVER_URL = "http://206.189.139.184/";
+    public static final String SERVER_URL = "http://206.189.139.184:8080/";
 }
