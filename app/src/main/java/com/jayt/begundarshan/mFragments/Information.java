fbb37d01fb29c25f9da946d78e37d257fd2a27c1
Jay Tailor <jayt@unity3d.com>
2018-11-26 22:37:40 -0800

    added links to news website and important numbers

--- app/src/main/java/com/jayt/begundarshan/mFragments/Information.java
+++ app/src/main/java/com/jayt/begundarshan/mFragments/Information.java
@@ -1,18 +1,25 @@
 package com.jayt.begundarshan.mFragments;
 
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.jayt.begundarshan.MainActivity;
 import com.jayt.begundarshan.R;
 
 public class Information extends Fragment {
 
     View view;
+    ImageButton bhaskar, navjyoti, jagran, patrika;
 
     public Information() {
     }
@@ -21,6 +28,69 @@ public class Information extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.information, container, false);
+
+        // Find the image buttons
+        bhaskar = (ImageButton) view.findViewById(R.id.dainikBhaskar);
+        navjyoti = (ImageButton) view.findViewById(R.id.dainikNavjyoti);
+        jagran = (ImageButton) view.findViewById(R.id.dainikJagran);
+        patrika = (ImageButton) view.findViewById(R.id.rajPatrika);
+
+        bhaskar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://www.bhaskar.com"));
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        navjyoti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("http://www.dainiknavajyoti.in"));
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        jagran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://www.jagran.com/"));
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        patrika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://www.patrika.com/rajasthan-news"));
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         return view;
     }
 
